18d64285b56490b6b41d774b79649cfc4c4812f7
Aleric Cusher <alericcusher25@gmail.com>
2023-10-06 13:27:19 +0530

    Adds functionality to handle for when player1 cooperates and player2 cheats

--- tests/test_trust_game.py
+++ tests/test_trust_game.py
@@ -1,12 +1,17 @@
 import pytest
 from trust.trust_game import play_game, TrustGameActions
 
-def test_cheat_cheat():
+def test_p1_cheat_p2_cheat():
     player1_outcome, player2_outcome = play_game(TrustGameActions.CHEAT, TrustGameActions.CHEAT)
     assert player1_outcome == 0
     assert player2_outcome == 0
 
-def test_cheat_cooperate():
+def test_p1_cheat_p2_cooperate():
     player1_outcome, player2_outcome = play_game(TrustGameActions.CHEAT, TrustGameActions.COOPERATE)
     assert player1_outcome == 3
     assert player2_outcome == -1
+
+def test_p1_cooperate_p2_cheat():
+    player1_outcome, player2_outcome = play_game(TrustGameActions.COOPERATE, TrustGameActions.CHEAT)
+    assert player1_outcome == -1
+    assert player2_outcome == 3
--- trust/trust_game.py
+++ trust/trust_game.py
@@ -14,3 +14,8 @@ def play_game(player1_choice, player2_choice):
         player1_outcome = 3
         player2_outcome = -1
         return player1_outcome, player2_outcome
+    elif player1_choice == TrustGameActions.COOPERATE and player2_choice == TrustGameActions.CHEAT:
+        player1_outcome = -1
+        player2_outcome = 3
+        return player1_outcome, player2_outcome
+    
