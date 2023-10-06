2e16cbe19af296e0245b5841a0c029e98f5f94e5
Aleric Cusher <alericcusher25@gmail.com>
2023-10-06 18:43:18 +0530

    Refactors test_trust_game tests

--- tests/test_trust_game.py
+++ tests/test_trust_game.py
@@ -4,7 +4,7 @@ from trust.actions import TrustGameActions
 from trust.players import AlwaysCooperatePlayer, RandomPlayer
 
 def mock_action_cheat():
-    return TrustGameActions.CHEAT
+        return TrustGameActions.CHEAT
 
 def mock_action_cooperate():
     return TrustGameActions.COOPERATE
@@ -21,60 +21,42 @@ def test_invalid_num_games_in_play_game():
     with pytest.raises(TypeError):
         play_game(RandomPlayer(), RandomPlayer(), 'a')
 
-def test_p1_cheat_p2_cheat(monkeypatch):
-    player1, player2 = RandomPlayer(), RandomPlayer()
 
-    monkeypatch.setattr(player1, 'action', mock_action_cheat)
-    monkeypatch.setattr(player2, 'action', mock_action_cheat)
+class TestAllActionCombinations:
+    all_possible_combination_parameters = [
+        # (player1_action_func, player2_action_func, player1_score, player2_score)
+        (mock_action_cheat, mock_action_cheat, 0, 0),
+        (mock_action_cheat, mock_action_cooperate, 3, -1),
+        (mock_action_cooperate, mock_action_cheat, -1, 3),
+        (mock_action_cooperate, mock_action_cooperate, 2, 2)
+    ]
 
-    play_game(player1, player2)
-    assert player1.score == 0
-    assert player2.score == 0
+    @pytest.mark.parametrize('player1_action_func, player2_action_func, player1_score, player2_score', all_possible_combination_parameters)
+    def test_all_action_combinations(self, monkeypatch, player1_action_func, player2_action_func, player1_score, player2_score):
+        player1, player2 = RandomPlayer(), RandomPlayer()
 
-def test_p1_cheat_p2_cooperate(monkeypatch):
-    player1, player2 = RandomPlayer(), RandomPlayer()
+        monkeypatch.setattr(player1, 'action', player1_action_func)
+        monkeypatch.setattr(player2, 'action', player2_action_func)
 
-    monkeypatch.setattr(player1, 'action', mock_action_cheat)
-    monkeypatch.setattr(player2, 'action', mock_action_cooperate)
+        play_game(player1, player2)
+        assert player1.score == player1_score
+        assert player2.score == player2_score
 
-    play_game(player1, player2)
-    assert player1.score == 3
-    assert player2.score == -1
+class TestGamesBetweenTwoPlayers:
+    def test_always_cooperate_player_games(self):
+        player1, player2 = AlwaysCooperatePlayer(), AlwaysCooperatePlayer()
+        play_game(player1, player2)
+        assert player1.score == 2
+        assert player2.score == 2
 
-def test_p1_cooperate_p2_cheat(monkeypatch):
-    player1, player2 = RandomPlayer(), RandomPlayer()
+    def test_always_cooperate_player_10_games(self):
+        player1, player2 = AlwaysCooperatePlayer(), AlwaysCooperatePlayer()
+        play_game(player1, player2, num_games=5)
+        assert player1.score == 10
+        assert player2.score == 10
 
-    monkeypatch.setattr(player1, 'action', mock_action_cooperate)
-    monkeypatch.setattr(player2, 'action', mock_action_cheat)
-
-    play_game(player1, player2)
-    assert player1.score == -1
-    assert player2.score == 3
-
-def test_p1_cooperate_p2_cooperate(monkeypatch):
-    player1, player2 = RandomPlayer(), RandomPlayer()
-
-    monkeypatch.setattr(player1, 'action', mock_action_cooperate)
-    monkeypatch.setattr(player2, 'action', mock_action_cooperate)
-
-    play_game(player1, player2)
-    assert player1.score == 2
-    assert player2.score == 2
-
-def test_always_cooperate_player_games():
-    player1, player2 = AlwaysCooperatePlayer(), AlwaysCooperatePlayer()
-    play_game(player1, player2)
-    assert player1.score == 2
-    assert player2.score == 2
-
-def test_always_cooperate_player_10_games():
-    player1, player2 = AlwaysCooperatePlayer(), AlwaysCooperatePlayer()
-    play_game(player1, player2, num_games=5)
-    assert player1.score == 10
-    assert player2.score == 10
-
-def test_always_cooperate_player_10_games():
-    player1, player2 = AlwaysCooperatePlayer(), AlwaysCooperatePlayer()
-    play_game(player1, player2, num_games=10)
-    assert player1.score == 20
-    assert player2.score == 20
+    def test_always_cooperate_player_10_games(self):
+        player1, player2 = AlwaysCooperatePlayer(), AlwaysCooperatePlayer()
+        play_game(player1, player2, num_games=10)
+        assert player1.score == 20
+        assert player2.score == 20
