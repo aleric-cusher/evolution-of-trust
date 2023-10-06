1fb0102b5bcf05478dc16c0d6dd32afcede42d9a
Aleric Cusher <alericcusher25@gmail.com>
2023-10-06 18:16:08 +0530

    Adds a new player: AlwaysCheatPlayer, and the corresponding tests

--- tests/test_player.py
+++ tests/test_player.py
@@ -1,7 +1,8 @@
 import pytest
 import random
-from trust.players import AlwaysCooperatePlayer, RandomPlayer
+from trust.players import AlwaysCooperatePlayer, RandomPlayer, AlwaysCheatPlayer
 from trust.trust_game import TrustGameActions
+
 
 class TestRandomPlayer:
     def test_creation(self):
@@ -14,6 +15,8 @@ class TestRandomPlayer:
         assert player.action() == TrustGameActions.COOPERATE
         assert player.action() == TrustGameActions.COOPERATE
         assert player.action() == TrustGameActions.CHEAT
+        assert player.action() == TrustGameActions.COOPERATE
+
 
 class TestAlwaysCooperatePlayer:
     def test_creation(self):
@@ -23,3 +26,19 @@ class TestAlwaysCooperatePlayer:
     def test_player_action(self):
         player = AlwaysCooperatePlayer()
         assert player.action() == TrustGameActions.COOPERATE
+        assert player.action() == TrustGameActions.COOPERATE
+        assert player.action() == TrustGameActions.COOPERATE
+        assert player.action() == TrustGameActions.COOPERATE
+
+    
+class TestAlwaysCheatPlayer:
+    def test_creation(self):
+        player = AlwaysCheatPlayer()
+        assert player.score == 0
+
+    def test_player_action(self):
+        player = AlwaysCheatPlayer()
+        assert player.action() == TrustGameActions.CHEAT
+        assert player.action() == TrustGameActions.CHEAT
+        assert player.action() == TrustGameActions.CHEAT
+        assert player.action() == TrustGameActions.CHEAT
--- trust/players.py
+++ trust/players.py
@@ -2,6 +2,7 @@ from trust.actions import TrustGameActions
 import random
 from abc import ABC, abstractmethod
 
+
 class BasePlayer(ABC):
     def __init__(self) -> None:
         self.score = 0
@@ -10,11 +11,17 @@ class BasePlayer(ABC):
     def action(self):
         ...
 
+
 class RandomPlayer(BasePlayer):
     def action(self) -> TrustGameActions:
         return random.choice([i for i in TrustGameActions])
+
 
 class AlwaysCooperatePlayer(BasePlayer):
     def action(self) -> TrustGameActions:
         return TrustGameActions.COOPERATE
     
+
+class AlwaysCheatPlayer(BasePlayer):
+    def action(self) -> TrustGameActions:
+        return TrustGameActions.CHEAT
