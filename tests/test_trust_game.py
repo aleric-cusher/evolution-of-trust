abffc87d84edff5cca1f0d9f272bd44f1eb5d8af
Aleric Cusher <alericcusher25@gmail.com>
2023-10-07 01:24:51 +0530

    Refactors TestAllActionCombinations to use newer player classes

--- tests/test_trust_game.py
+++ tests/test_trust_game.py
@@ -30,23 +30,18 @@ class TestPlayGame:
             assert False, f'Exception {e}'
 
 
-
 class TestAllActionCombinations:
-    all_possible_combination_parameters = [
-        # (player1_action_func, player2_action_func, player1_score, player2_score)
-        (mock_action_cheat, mock_action_cheat, 0, 0),
-        (mock_action_cheat, mock_action_cooperate, 3, -1),
-        (mock_action_cooperate, mock_action_cheat, -1, 3),
-        (mock_action_cooperate, mock_action_cooperate, 2, 2)
+    combination_parameters = [
+        # (player1_class, player2_class, player1_score, player2_score)
+        (AlwaysCheatPlayer, AlwaysCheatPlayer, 0, 0),
+        (AlwaysCheatPlayer, AlwaysCooperatePlayer, 3, -1),
+        (AlwaysCooperatePlayer, AlwaysCheatPlayer, -1, 3),
+        (AlwaysCooperatePlayer, AlwaysCooperatePlayer, 2, 2)
     ]
 
-    @pytest.mark.parametrize('player1_action_func, player2_action_func, player1_score, player2_score', all_possible_combination_parameters)
-    def test_all_action_combinations(self, monkeypatch, player1_action_func, player2_action_func, player1_score, player2_score):
-        player1, player2 = RandomPlayer(), RandomPlayer()
-
-        monkeypatch.setattr(player1, 'action', player1_action_func)
-        monkeypatch.setattr(player2, 'action', player2_action_func)
-
+    @pytest.mark.parametrize('player1_class, player2_class, player1_score, player2_score', combination_parameters)
+    def test_action_combinations(self, player1_class, player2_class, player1_score, player2_score):
+        player1, player2 = player1_class(), player2_class()
         play_game(player1, player2)
         assert player1.score == player1_score
         assert player2.score == player2_score
