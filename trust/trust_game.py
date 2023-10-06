0e84b99c1c49d43248ff7b414a156ec93835a2c1
Aleric Cusher <alericcusher25@gmail.com>
2023-10-06 13:38:42 +0530

    Refactors play_game method in trust_game file to reduce code duplication
    Uses outcomes dict to map the choices to outcomes

--- trust/trust_game.py
+++ trust/trust_game.py
@@ -4,22 +4,13 @@ class TrustGameActions(Enum):
     CHEAT = auto()
     COOPERATE = auto()
 
+outcomes = {
+    (TrustGameActions.CHEAT, TrustGameActions.CHEAT): (0, 0),
+    (TrustGameActions.CHEAT, TrustGameActions.COOPERATE): (3, -1),
+    (TrustGameActions.COOPERATE, TrustGameActions.CHEAT): (-1, 3),
+    (TrustGameActions.COOPERATE, TrustGameActions.COOPERATE): (2, 2),    
+}
 
 def play_game(player1_choice, player2_choice):
-    if player1_choice == TrustGameActions.CHEAT and player2_choice == TrustGameActions.CHEAT:
-        player1_outcome = 0
-        player2_outcome = 0
-        return player1_outcome, player2_outcome
-    elif player1_choice == TrustGameActions.CHEAT and player2_choice == TrustGameActions.COOPERATE:
-        player1_outcome = 3
-        player2_outcome = -1
-        return player1_outcome, player2_outcome
-    elif player1_choice == TrustGameActions.COOPERATE and player2_choice == TrustGameActions.CHEAT:
-        player1_outcome = -1
-        player2_outcome = 3
-        return player1_outcome, player2_outcome
-    elif player1_choice == TrustGameActions.COOPERATE and player2_choice == TrustGameActions.COOPERATE:
-        player1_outcome = 2
-        player2_outcome = 2
-        return player1_outcome, player2_outcome
+    return outcomes[(player1_choice, player2_choice)]
     
