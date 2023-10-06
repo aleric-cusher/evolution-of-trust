cfde0bbaac08750f890651f2be3f4f4de646bdfa
Aleric Cusher <alericcusher25@gmail.com>
2023-10-06 13:20:37 +0530

    Adds functionality to handle when first player cooperates and second doesnt

--- tests/test_trust_game.py
+++ tests/test_trust_game.py
@@ -5,3 +5,8 @@ def test_cheat_cheat():
     player1_outcome, player2_outcome = play_game(TrustGameActions.CHEAT, TrustGameActions.CHEAT)
     assert player1_outcome == 0
     assert player2_outcome == 0
+
+def test_cheat_cooperate():
+    player1_outcome, player2_outcome = play_game(TrustGameActions.CHEAT, TrustGameActions.COOPERATE)
+    assert player1_outcome == 3
+    assert player2_outcome == -1
--- trust/trust_game.py
+++ trust/trust_game.py
@@ -1,11 +1,16 @@
-from enum import Enum
+from enum import Enum, auto
 
 class TrustGameActions(Enum):
-    CHEAT = 0
+    CHEAT = auto()
+    COOPERATE = auto()
 
 
 def play_game(player1_choice, player2_choice):
     if player1_choice == TrustGameActions.CHEAT and player2_choice == TrustGameActions.CHEAT:
         player1_outcome = 0
         player2_outcome = 0
+        return player1_outcome, player2_outcome
+    elif player1_choice == TrustGameActions.CHEAT and player2_choice == TrustGameActions.COOPERATE:
+        player1_outcome = 3
+        player2_outcome = -1
         return player1_outcome, player2_outcome
