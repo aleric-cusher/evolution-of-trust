7447ac651fa8e01191d46796012c483afe33e5f5
Aleric Cusher <alericcusher25@gmail.com>
2023-10-06 17:02:52 +0530

    Adds BasePlayer abstract class to reduce duplication

--- trust/player.py
+++ trust/player.py
@@ -1,16 +1,19 @@
-from trust.trust_game import TrustGameActions
+from trust.actions import TrustGameActions
 import random
+from abc import ABC, abstractmethod
 
-class Player:
-    def __init__(self) -> None:
-        self.score = 0
-
-    def action(self) -> TrustGameActions:
-        return random.choice([i for i in TrustGameActions])
-
-class AlwaysCooperate:
+class BasePlayer(ABC):
     def __init__(self) -> None:
         self.score = 0
     
+    @abstractmethod
+    def action(self):
+        ...
+
+class Player(BasePlayer):
+    def action(self) -> TrustGameActions:
+        return random.choice([i for i in TrustGameActions])
+
+class AlwaysCooperate(BasePlayer):
     def action(self) -> TrustGameActions:
         return TrustGameActions.COOPERATE
