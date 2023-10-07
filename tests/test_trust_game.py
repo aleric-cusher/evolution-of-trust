8aad0ae90457438acba197a2b84f7ff7004b3260
Aleric Cusher <alericcusher25@gmail.com>
2023-10-07 15:25:10 +0530

    Encapsulates play_game in a TrustGame class and refactors corresponding tests

--- tests/test_trust_game.py
+++ tests/test_trust_game.py
@@ -1,28 +1,40 @@
 import pytest
 import random
-from trust.trust_game import play_game
+from trust.trust_game import TrustGame
 from trust.actions import TrustGameActions
 from trust.players import AlwaysCooperatePlayer, RandomPlayer, AlwaysCheatPlayer
 
 
 class TestPlayGame:
-    def test_invalid_player_types_in_play_game(self):
-        with pytest.raises(TypeError):
-            play_game('a', 4)
-
-    def test_invalid_num_games_in_play_game(self):
-        with pytest.raises(ValueError):
-            play_game(RandomPlayer(), RandomPlayer(), 0)
-
-    def test_invalid_num_games_in_play_game(self):
-        with pytest.raises(TypeError):
-            play_game(RandomPlayer(), RandomPlayer(), 'a')
-    
     def test_valid_run(self):
         try:
-            play_game(AlwaysCooperatePlayer(), AlwaysCooperatePlayer())
+            game = TrustGame(AlwaysCooperatePlayer(), AlwaysCooperatePlayer())
+            game.play_game()
         except Exception as e:
             assert False, f'Exception {e}'
+    
+    def test_correct_attribute_initialization(self):
+        player1_instance, player2_instance = RandomPlayer(), RandomPlayer()
+        game = TrustGame(player1_instance, player2_instance)
+        
+        assert game.player1 == player1_instance
+        assert game.player2 == player2_instance
+        assert game.player1_score == 0
+        assert game.player2_score == 0
+
+    def test_invalid_player_types_in_trust_game(self):
+        with pytest.raises(TypeError):
+            TrustGame('a', 4)
+
+    def test_invalid_num_games_in_play_game(self):
+        game = TrustGame(RandomPlayer(), RandomPlayer())
+        with pytest.raises(ValueError):
+            game.play_game(0)
+
+    def test_invalid_num_games_type_in_play_game(self):
+        game = TrustGame(RandomPlayer(), RandomPlayer())
+        with pytest.raises(TypeError):
+            game.play_game('a')
 
 
 class TestAllActionCombinations:
@@ -37,47 +49,54 @@ class TestAllActionCombinations:
     @pytest.mark.parametrize('player1_class, player2_class, player1_score, player2_score', combination_parameters)
     def test_action_combinations(self, player1_class, player2_class, player1_score, player2_score):
         player1, player2 = player1_class(), player2_class()
-        play_game(player1, player2)
-        assert player1.score == player1_score
-        assert player2.score == player2_score
+        game = TrustGame(player1, player2)
+        game.play_game()
+        assert game.player1_score == player1_score
+        assert game.player2_score == player2_score
 
 
 class TestGamesBetweenSamePlayers:
     def test_always_cooperate_player_games(self):
         player1, player2 = AlwaysCooperatePlayer(), AlwaysCooperatePlayer()
-        play_game(player1, player2)
-        assert player1.score == 2
-        assert player2.score == 2
+        game = TrustGame(player1, player2)
+        game.play_game()
+        assert game.player1_score == 2
+        assert game.player2_score == 2
 
     def test_always_cooperate_player_5_games(self):
         player1, player2 = AlwaysCooperatePlayer(), AlwaysCooperatePlayer()
-        play_game(player1, player2, num_games=5)
-        assert player1.score == 10
-        assert player2.score == 10
+        game = TrustGame(player1, player2)
+        game.play_game(5)
+        assert game.player1_score == 10
+        assert game.player2_score == 10
 
     def test_always_cooperate_player_10_games(self):
         player1, player2 = AlwaysCooperatePlayer(), AlwaysCooperatePlayer()
-        play_game(player1, player2, num_games=10)
-        assert player1.score == 20
-        assert player2.score == 20
+        game = TrustGame(player1, player2)
+        game.play_game(10)
+        assert game.player1_score == 20
+        assert game.player2_score == 20
 
     def test_always_cheat_player_games(self):
         player1, player2 = AlwaysCheatPlayer(), AlwaysCheatPlayer()
-        play_game(player1, player2)
-        assert player1.score == 0
-        assert player2.score == 0
+        game = TrustGame(player1, player2)
+        game.play_game()
+        assert game.player1_score == 0
+        assert game.player2_score == 0
 
     def test_always_cheat_player_5_games(self):
         player1, player2 = AlwaysCheatPlayer(), AlwaysCheatPlayer()
-        play_game(player1, player2, num_games=5)
-        assert player1.score == 0
-        assert player2.score == 0
+        game = TrustGame(player1, player2)
+        game.play_game(5)
+        assert game.player1_score == 0
+        assert game.player2_score == 0
 
     def test_always_cheat_player_10_games(self):
         player1, player2 = AlwaysCheatPlayer(), AlwaysCheatPlayer()
-        play_game(player1, player2, num_games=10)
-        assert player1.score == 0
-        assert player2.score == 0
+        game = TrustGame(player1, player2)
+        game.play_game(10)
+        assert game.player1_score == 0
+        assert game.player2_score == 0
     
 
 class TestGamesBetweenDifferentPlayers:
@@ -86,15 +105,17 @@ class TestGamesBetweenDifferentPlayers:
     
     def test_alwayscooperate_alwayscheat_10_games(self):
         player1, player2 = AlwaysCooperatePlayer(), AlwaysCheatPlayer()
-        play_game(player1, player2, num_games=10)
-        assert player1.score == -10
-        assert player2.score == 30
+        game = TrustGame(player1, player2)
+        game.play_game(10)
+        assert game.player1_score == -10
+        assert game.player2_score == 30
     
     def test_alwayscooperate_alwayscheat_10_games_opposite(self):
         player1, player2 = AlwaysCheatPlayer(), AlwaysCooperatePlayer()
-        play_game(player1, player2, num_games=10)
-        assert player1.score == 30
-        assert player2.score == -10
+        game = TrustGame(player1, player2)
+        game.play_game(10)
+        assert game.player1_score == 30
+        assert game.player2_score == -10
     
     parameters = [
         # (player1_class, player2_class, num_games, player1_score, player2_score)
@@ -108,8 +129,9 @@ class TestGamesBetweenDifferentPlayers:
         spy = mocker.spy(random, 'choice')
 
         player1, player2 = player1_class(), player2_class()
-        play_game(player1, player2, num_games=num_games)
+        game = TrustGame(player1, player2)
+        game.play_game(num_games)
 
-        assert player1.score == player1_score
-        assert player2.score == player2_score
+        assert game.player1_score == player1_score
+        assert game.player2_score == player2_score
         assert spy.call_count == num_games
--- trust/trust_game.py
+++ trust/trust_game.py
@@ -1,24 +1,36 @@
 from trust.players import BasePlayer
 from trust.actions import TrustGameActions
 
-outcomes = {
-    (TrustGameActions.CHEAT, TrustGameActions.CHEAT): (0, 0),
-    (TrustGameActions.CHEAT, TrustGameActions.COOPERATE): (3, -1),
-    (TrustGameActions.COOPERATE, TrustGameActions.CHEAT): (-1, 3),
-    (TrustGameActions.COOPERATE, TrustGameActions.COOPERATE): (2, 2),    
-}
 
-def play_game(player1: BasePlayer, player2: BasePlayer, num_games: int = 1) -> None:
-    if not (isinstance(player1, BasePlayer) and isinstance(player2, BasePlayer)):
-        raise TypeError('Invalid player class.')
+class TrustGame:
+    outcomes = {
+        (TrustGameActions.CHEAT, TrustGameActions.CHEAT): (0, 0),
+        (TrustGameActions.CHEAT, TrustGameActions.COOPERATE): (3, -1),
+        (TrustGameActions.COOPERATE, TrustGameActions.CHEAT): (-1, 3),
+        (TrustGameActions.COOPERATE, TrustGameActions.COOPERATE): (2, 2),    
+    }
+
+    def __init__(self, player1: BasePlayer, player2: BasePlayer) -> None:
+        if not (isinstance(player1, BasePlayer) and isinstance(player2, BasePlayer)):
+            raise TypeError('Invalid player class.')
+
+        self.player1 = player1
+        self.player2 = player2
+        self.player1_score = 0
+        self.player2_score = 0
+
+    def update_player_score(self, scores):
+        self.player1_score += scores[0]
+        self.player2_score += scores[1]
     
-    if not isinstance(num_games, int):
-        raise TypeError('num_games should be of type int.')
-    if num_games < 1:
-        raise ValueError('Cannot play 0 or negative games.')
-    
-    for _ in range(num_games):
-        player1_outcome, player2_outcome =  outcomes[(player1.action(), player2.action())]
-        player1.score += player1_outcome
-        player2.score += player2_outcome
-    
+    def play_game(self, num_games: int = 1) -> None:
+        if not isinstance(num_games, int):
+            raise TypeError('num_games should be of type int.')
+        if num_games < 1:
+            raise ValueError('Cannot play 0 or negative games.')
+        
+        for _ in range(num_games):
+            scores =  self.outcomes[(self.player1.action(), self.player2.action())]
+            self.update_player_score(scores)
+            
+        
