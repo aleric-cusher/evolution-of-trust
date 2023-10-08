7a1b278f706460924c0d6892e83c993a35831905
Aleric Cusher <alericcusher25@gmail.com>
2023-10-09 01:12:44 +0530

    Adds scorecard parameter to action method and implements CopycatPlayer

--- tests/test_player.py
+++ tests/test_player.py
@@ -1,6 +1,6 @@
 import pytest
 import random
-from trust.players import AlwaysCooperatePlayer, RandomPlayer, AlwaysCheatPlayer
+from trust.players import AlwaysCooperatePlayer, RandomPlayer, AlwaysCheatPlayer, CopycatPlayer
 from trust.trust_game import TrustGameActions
 
 
@@ -27,3 +27,26 @@ class TestAlwaysCheatPlayer:
     def test_player_action(self):
         player = AlwaysCheatPlayer()
         assert player.action() == TrustGameActions.CHEAT
+
+
+class TestCopycatplayer:
+    def test_player_action(self):
+        player1 = CopycatPlayer()
+        player2 = CopycatPlayer()
+        scorecard = {
+            player1: {'score': 0, 'actions': [TrustGameActions.COOPERATE]},
+            player2: {'score': 0, 'actions': [TrustGameActions.CHEAT]}
+        }
+        assert player1.action(scorecard) == TrustGameActions.CHEAT
+        assert player2.action(scorecard) == TrustGameActions.COOPERATE
+
+    def test_player_action_empty_scorecard(self):
+        player1 = CopycatPlayer()
+        player2 = CopycatPlayer()
+        scorecard = {
+            player1: {'score': 0, 'actions': []},
+            player2: {'score': 0, 'actions': []}
+        }
+        assert player1.action(scorecard) == TrustGameActions.COOPERATE
+        assert player2.action(scorecard) == TrustGameActions.COOPERATE
+
--- trust/players.py
+++ trust/players.py
@@ -1,24 +1,36 @@
+from __future__ import annotations
 from trust.actions import TrustGameActions
+from typing import Optional
 import random
 from abc import ABC, abstractmethod
 
 
 class BasePlayer(ABC):
     @abstractmethod
-    def action(self):
+    def action(self, scorecard:dict[BasePlayer, dict[str, any]] = None):
         ...
 
 
 class RandomPlayer(BasePlayer):
-    def action(self) -> TrustGameActions:
+    def action(self, scorecard:dict[BasePlayer, dict[str, any]] = None) -> TrustGameActions:
         return random.choice([i for i in TrustGameActions])
 
 
 class AlwaysCooperatePlayer(BasePlayer):
-    def action(self) -> TrustGameActions:
+    def action(self, scorecard:dict[BasePlayer, dict[str, any]] = None) -> TrustGameActions:
         return TrustGameActions.COOPERATE
     
 
 class AlwaysCheatPlayer(BasePlayer):
-    def action(self) -> TrustGameActions:
+    def action(self, scorecard:dict[BasePlayer, dict[str, any]] = None) -> TrustGameActions:
         return TrustGameActions.CHEAT
+
+
+class CopycatPlayer(BasePlayer):
+    def action(self, scorecard:dict[BasePlayer, dict[str, any]]):
+        other_player = [player for player in scorecard.keys() if player != self][0]
+        other_player_actions = scorecard[other_player]['actions']
+        if len(other_player_actions) < 1:
+            return TrustGameActions.COOPERATE
+        return other_player_actions[-1]
+        
