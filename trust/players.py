bada51683ab2d325411e3035bb6c90895eb081ea
Aleric Cusher <alericcusher25@gmail.com>
2023-10-09 09:29:20 +0530

    Improves typing hint in player module

--- trust/players.py
+++ trust/players.py
@@ -1,36 +1,35 @@
 from __future__ import annotations
 from trust.actions import TrustGameActions
-from typing import Optional
+from typing import Optional, Any
 import random
 from abc import ABC, abstractmethod
 
 
 class BasePlayer(ABC):
     @abstractmethod
-    def action(self, scorecard:dict[BasePlayer, dict[str, any]] = None):
+    def action(self, scorecard:Optional[dict[BasePlayer, dict[str, Any]]] = None):
         ...
 
 
 class RandomPlayer(BasePlayer):
-    def action(self, scorecard:dict[BasePlayer, dict[str, any]] = None) -> TrustGameActions:
+    def action(self, scorecard:Optional[dict[BasePlayer, dict[str, Any]]] = None) -> TrustGameActions:
         return random.choice([i for i in TrustGameActions])
 
 
 class AlwaysCooperatePlayer(BasePlayer):
-    def action(self, scorecard:dict[BasePlayer, dict[str, any]] = None) -> TrustGameActions:
+    def action(self, scorecard:Optional[dict[BasePlayer, dict[str, Any]]] = None) -> TrustGameActions:
         return TrustGameActions.COOPERATE
     
 
 class AlwaysCheatPlayer(BasePlayer):
-    def action(self, scorecard:dict[BasePlayer, dict[str, any]] = None) -> TrustGameActions:
+    def action(self, scorecard:Optional[dict[BasePlayer, dict[str, Any]]] = None) -> TrustGameActions:
         return TrustGameActions.CHEAT
 
 
 class CopycatPlayer(BasePlayer):
-    def action(self, scorecard:dict[BasePlayer, dict[str, any]]):
+    def action(self, scorecard:dict[BasePlayer, dict[str, Any]]):
         other_player = [player for player in scorecard.keys() if player != self][0]
         other_player_actions = scorecard[other_player]['actions']
         if len(other_player_actions) < 1:
             return TrustGameActions.COOPERATE
         return other_player_actions[-1]
-        
