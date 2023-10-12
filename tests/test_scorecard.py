6bccde9417956d2dbe1a70156918afe5ee0d1987
Aleric Cusher <alericcusher25@gmail.com>
2023-10-12 15:45:39 +0530

    Changes Scorecard to maintain player actions as well as scores

--- tests/test_scorecard.py
+++ tests/test_scorecard.py
@@ -8,109 +8,147 @@ class TestScorecard:
     def test_creation(self):
         players = [RandomPlayer(), AlwaysCheatPlayer(), AlwaysCooperatePlayer()]
         try:
-            score_handler = Scorecard(players)
+            scorecard = Scorecard(players)
             assert True
         except Exception as e:
             assert False, e
         
     def test_scorecard_attributes(self):
         player1 = RandomPlayer()
-        score_handler = Scorecard([player1])
+        scorecard = Scorecard([player1])
 
-        assert score_handler.scorecard[player1] == 0
+        assert player1 in scorecard._scorecard.keys()
 
-    def test_scorecard_update_score(self):
-        player1, player2 = AlwaysCheatPlayer(), AlwaysCooperatePlayer()
-        score_handler = Scorecard([player1, player2]) 
+    def test_get_score(self):
+        player1, player2, player3 = AlwaysCheatPlayer(), AlwaysCooperatePlayer(), RandomPlayer()
+        scorecard = Scorecard([player1, player2, player3])
+        test_card = {
+            player1: {'score': 5, 'actions': []},
+            player2: {'score': 6, 'actions': []},
+            player3: {'score': 2, 'actions': []},
+        }
 
-        score_handler.update_scores(33, player1)
-        score_handler.update_scores(25, player2)
+        scorecard._scorecard = test_card
         
-        assert score_handler.scorecard[player1] == 33
-        assert score_handler.scorecard[player2] == 25
+        assert scorecard.get_score(player2) == 6
+        assert scorecard.get_score(player3) == 2
+        assert scorecard.get_score(player1) == 5
+
+    def test_update_score(self):
+        player1, player2 = AlwaysCheatPlayer(), AlwaysCooperatePlayer()
+        scorecard = Scorecard([player1, player2]) 
+
+        scorecard.update_score(33, player1)
+        scorecard.update_score(25, player2)
+        
+        assert scorecard.get_score(player1) == 33
+        assert scorecard.get_score(player2) == 25
         
     def test_reset_score(self):
         player1, player2 = AlwaysCheatPlayer(), AlwaysCooperatePlayer()
-        score_handler = Scorecard([player1, player2])
-        score_handler.scorecard = {
-            player1: 5,
-            player2: 6
-        }
-        score_handler.reset_score(player1)
+        scorecard = Scorecard([player1, player2])
+        
+        scorecard.update_score(45, player1)
+        scorecard.update_score(6, player2)
+        scorecard.reset_score(player1)
 
-        assert score_handler.scorecard[player1] == 0
-        assert score_handler.scorecard[player2] == 6
+        assert scorecard.get_score(player1) == 0
+        assert scorecard.get_score(player2) == 6
 
-    def test_reset_scorecard(self):
-        player1, player2 = AlwaysCheatPlayer(), AlwaysCooperatePlayer()
-        score_handler = Scorecard([player1, player2])
-        score_handler.scorecard = {
-            player1: 5,
-            player2: 6
-        }
-
-        score_handler.reset_scorecard()
-
-        assert score_handler.scorecard[player1] == 0
-        assert score_handler.scorecard[player2] == 0
-    
-    def test_get_scores(self):
+    def test_get_actions(self):
         player1, player2, player3 = AlwaysCheatPlayer(), AlwaysCooperatePlayer(), RandomPlayer()
-        score_handler = Scorecard([player1, player2, player3])
+        scorecard = Scorecard([player1, player2, player3])
         test_card = {
-            player1: 5,
-            player2: 6,
-            player3: 2,
+            player1: {'score': 5, 'actions': [TrustGameActions.CHEAT, TrustGameActions.CHEAT]},
+            player2: {'score': 6, 'actions': [TrustGameActions.COOPERATE, TrustGameActions.COOPERATE]},
+            player3: {'score': 2, 'actions': [TrustGameActions.COOPERATE, TrustGameActions.CHEAT]},
         }
 
-        score_handler.scorecard = test_card
+        scorecard._scorecard = test_card
+        
+        assert scorecard.get_actions(player2) == [TrustGameActions.COOPERATE, TrustGameActions.COOPERATE]
+        assert scorecard.get_actions(player3) == [TrustGameActions.COOPERATE, TrustGameActions.CHEAT]
+        assert scorecard.get_actions(player1) == [TrustGameActions.CHEAT, TrustGameActions.CHEAT]
 
-        scorecard_object = score_handler.get_scores([player3, player2])
-        assert scorecard_object[player2] == 6
-        assert scorecard_object[player3] == 2
-        assert player1 not in scorecard_object.keys()
+    def test_update_actions(self):
+        player1, player2 = AlwaysCheatPlayer(), AlwaysCooperatePlayer()
+        scorecard = Scorecard([player1, player2]) 
+
+        scorecard.update_actions(TrustGameActions.CHEAT, player1)
+        scorecard.update_actions(TrustGameActions.COOPERATE, player2)
+        
+        assert scorecard.get_actions(player1) == [TrustGameActions.CHEAT]
+        assert scorecard.get_actions(player2) == [TrustGameActions.COOPERATE]
+
+    def test_reset_actions(self):
+        player1, player2 = AlwaysCheatPlayer(), AlwaysCooperatePlayer()
+        scorecard = Scorecard([player1, player2]) 
+
+        scorecard.update_actions(TrustGameActions.CHEAT, player1)
+        scorecard.update_actions(TrustGameActions.COOPERATE, player2)
+        
+        scorecard.reset_actions(player1)
+        
+        assert scorecard.get_actions(player1) == []
+        assert scorecard.get_actions(player2) == [TrustGameActions.COOPERATE]
     
     def test_get_scorecard(self):
         player1, player2, player3 = AlwaysCheatPlayer(), AlwaysCooperatePlayer(), RandomPlayer()
-        score_handler = Scorecard([player1, player2, player3])
+        scorecard = Scorecard([player1, player2, player3])
         test_card = {
-            player1: 5,
-            player2: 6,
-            player3: 2,
+            player1: {'score': 5, 'actions': [TrustGameActions.CHEAT, TrustGameActions.CHEAT]},
+            player2: {'score': 6, 'actions': [TrustGameActions.COOPERATE, TrustGameActions.COOPERATE]},
+            player3: {'score': 2, 'actions': [TrustGameActions.COOPERATE, TrustGameActions.CHEAT]},
         }
 
-        score_handler.scorecard = test_card
-        scorecard_object = score_handler.get_scorecard()
+        scorecard._scorecard = test_card
+        scorecard_object = scorecard.get_scorecard()
         
         assert scorecard_object == test_card
+        
+    def test_reset_scorecard(self):
+        player1, player2 = AlwaysCheatPlayer(), AlwaysCooperatePlayer()
+        scorecard = Scorecard([player1, player2])
+        test_card = {
+            player1: {'score': 5, 'actions': [TrustGameActions.CHEAT, TrustGameActions.CHEAT]},
+            player2: {'score': 6, 'actions': [TrustGameActions.COOPERATE, TrustGameActions.COOPERATE]},
+        }
+
+        scorecard._scorecard = test_card
+        scorecard.reset_scorecard()
+
+        assert scorecard._scorecard == {
+            player1: {'score': 0, 'actions': []},
+            player2: {'score': 0, 'actions': []},
+        }
 
     def test_add_scores(self):
         player1, player2, player3 = AlwaysCheatPlayer(), AlwaysCooperatePlayer(), RandomPlayer()
-        score_handler1 = Scorecard([player1, player2, player3])
-        score_handler1.scorecard = {
-            player1: 5,
-            player2: 6,
-            player3: 2,
+        scorecard = Scorecard([player1, player2, player3])
+        scorecard._scorecard = {
+            player1: {'score': 5, 'actions': []},
+            player2: {'score': 6, 'actions': []},
+            player3: {'score': 2, 'actions': []},
         }
 
-        score_handler2 = Scorecard([player1, player3])
-        score_handler2.scorecard = {
-            player1: -3,
-            player3: 8
+        scorecard2 = Scorecard([player1, player3])
+        scorecard2._scorecard = {
+            player1: {'score': -3, 'actions': []},
+            player3: {'score': 8, 'actions': []}
         }
 
         expected_scorecard = {
-            player1: 2,
-            player2: 6,
-            player3: 10
+            player1: {'score': 2, 'actions': []},
+            player2: {'score': 6, 'actions': []},
+            player3: {'score': 10, 'actions': []}
         }
 
-        score_handler3 = score_handler1.add_score_handlers(score_handler2)
+        scorecard3 = scorecard.add_scores(scorecard2)
         
-        assert score_handler3.get_scorecard() == expected_scorecard
+        assert scorecard3.get_scorecard() == expected_scorecard
     
     def test_invalid_type_handler(self):
         player1, player2, player3 = AlwaysCheatPlayer(), AlwaysCooperatePlayer(), RandomPlayer()
-        score_handler1 = Scorecard([player1, player2, player3])
+        scorecard = Scorecard([player1, player2, player3])
         with pytest.raises(TypeError):
-            score_handler3 = score_handler1.add_score_handlers('score_handler2')
+            scorecard3 = scorecard.add_scores('score_handler2')
--- trust/scorecard.py
+++ trust/scorecard.py
@@ -1,35 +1,45 @@
-from copy import deepcopy
+from copy import deepcopy, copy
 from typing import Any, Dict, List
+from trust.actions import TrustGameActions
 from trust.players import BasePlayer
 
 
 class Scorecard:
     def __init__(self, players: List[BasePlayer]) -> None:
-        self.scorecard = {player: 0 for player in players}
+        self._scorecard = {player: {'score': 0, 'actions': []} for player in players}
 
-    def get_score(self, player: BasePlayer):
-        return self.scorecard.get(player, 0)
-    def update_scores(self, score: int, player: BasePlayer) -> None:
-        self.scorecard[player] += score
+    def get_card(self, player: BasePlayer):
+        return deepcopy(self._scorecard.get(player, None))
+
+    def get_score(self, player: BasePlayer) -> Dict[BasePlayer, Dict[str, Any]]:
+        return self._scorecard[player]['score']
+    
+    def update_score(self, score: int, player: BasePlayer) -> None:
+        self._scorecard[player]['score'] += score
 
     def reset_score(self, player: BasePlayer) -> None:
-        self.scorecard[player] = 0
-
-    def reset_scorecard(self):
-        self.scorecard = {player: 0 for player in self.scorecard.keys()}
-
-    def get_scores(self, players: List[BasePlayer]) -> Dict[BasePlayer, Dict[str, Any]]:
-        scorecard = {player: self.scorecard[player] for player in players}
-        return scorecard
+        self._scorecard[player]['score'] = 0
     
+    def get_actions(self, player: BasePlayer) -> List[TrustGameActions]:
+        return copy(self._scorecard[player]['actions'])
+    
+    def update_actions(self, action: TrustGameActions, player: BasePlayer) -> None:
+        self._scorecard[player]['actions'].append(action)
+
+    def reset_actions(self, player: BasePlayer) -> None:
+        self._scorecard[player]['actions'] = []
+
     def get_scorecard(self):
-        return self.scorecard
+        return copy(self._scorecard)
     
-    def add_score_handlers(self, other_score_handler):
-        if not isinstance(other_score_handler, Scorecard):
+    def reset_scorecard(self):
+        self._scorecard = {player: {'score': 0, 'actions': []} for player in self._scorecard.keys()}
+    
+    def add_scores(self, other_scorecard):
+        if not isinstance(other_scorecard, Scorecard):
             raise TypeError('Parameter should be of the type ScoreHandler')
 
-        for player in self.scorecard.keys():
-            self.scorecard[player] += other_score_handler.get_score(player)
+        for player in other_scorecard.get_scorecard().keys():
+            self.update_score(other_scorecard.get_score(player), player)
         
         return self
